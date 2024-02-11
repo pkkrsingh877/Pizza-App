d9e16f244b3392bc78721c629942a5f0e22820a7
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-02-11 08:52:39 +0530

    Added customer and order datatype and orderId generator function

--- main.c
+++ main.c
@@ -8,97 +8,35 @@ struct item{
     struct item *next;
 };
 
+// Structure to represent a customer
+struct customer {
+    char name[50];
+    char gender[10];
+    int age;
+    long int orderId; // Assuming orderId is of type long
+};
+
+// Structure to represent an Order
+struct order {
+    long int orderId;
+    char allItems[1000];
+    int totalCost;
+};
+
 struct item *head = NULL; // store address of first node
 struct item *temp = NULL; // traverse the items in cart
 struct item *newnode = NULL; // create new items
 struct item *removeNode = NULL; // remove nodes
 struct item *tail = NULL; // stores address of last node
 
-// Function to make the order
-void makeOrder() {
-    // Display the current cart
-    viewCart();
-
-    // Prompt the user to confirm the order
-    char choice;
-    printf("Do you want to make this order? (Y/N): ");
-    scanf(" %c", &choice);
-
-    // If the user confirms, display a message and clear the cart
-    if (choice == 'Y' || choice == 'y') {
-        printf("Thank you for your order!\n");
-        removeItems();
-    } else {
-        printf("Order canceled.\n");
-    }
-}
-
-void removeItems(){
-    temp = head;
-    while(temp != NULL){
-        removeNode = temp;
-        temp = temp -> next;
-        printf("Removing: %s\n", removeNode -> pizzaName);
-        free(removeNode);
-    }
-    head = tail = NULL; // Reset head and tail pointers
-}
-
-void viewCart(){
-    temp = head;
-    int price = 0;
-    printf("\n--------- Your Cart ---------\n");
-    printf("Name\t\tPrice\n");
-    printf("--------------------------------\n");
-    while(temp != NULL){
-        printf("%-20s %d\n", temp -> pizzaName, temp -> price);
-        price += temp -> price;
-        temp = temp -> next;
-    }
-    printf("--------------------------------\n");
-    printf("Total Cost: %d\n\n", price);
-}
-
-void addItemsToCart(char pizzaName[], int price){
-    newnode = (struct item*)malloc(sizeof(struct item));
-    strcpy(newnode -> pizzaName, pizzaName);
-    newnode -> price = price;
-    newnode -> next = NULL;
-
-    if(head == NULL){
-        head = tail = newnode;
-    }else{
-        tail -> next = newnode;
-        tail = newnode;
-    }
-}
-
-void mainMenu(){
-    printf("\n---------- Main Menu ----------\n");
-    printf("1. Veg Pizza\n");
-    printf("2. Non Veg Pizza\n");
-    printf("3. View Cart\n");
-    printf("4. Exit\n");
-    printf("------------------------------\n");
-}
-
-void vegMenu(){
-    printf("\n--------- Veg Pizza ---------\n");
-    printf("1. Cheese Pizza\n");
-    printf("2. Veggie Pizza\n");
-    printf("3. Pepperoni Pizza\n");
-    printf("4. Back to Main Menu\n");
-    printf("-----------------------------\n");
-}
-
-void nonVegMenu(){
-    printf("\n-------- Non Veg Pizza --------\n");
-    printf("1. BBQ Chicken Pizza\n");
-    printf("2. Meat Pizza\n");
-    printf("3. Gamberi Pizza\n");
-    printf("4. Back to Main Menu\n");
-    printf("-------------------------------\n");
-}
+void makeOrder();
+void removeItems();
+void viewCart();
+void addItemsToCart(char pizzaName[], int price);
+void mainMenu();
+void vegMenu();
+void nonVegMenu();
+long int generateOrderId();
 
 int main(){
     int keepGoing = 1, choice;
@@ -165,10 +103,10 @@ int main(){
                 break;
             case 5:
                 printf("Thank you for visiting!\n");
-                break;
+                exit(1);
             default: 
                 printf("Invalid choice.\n");
-                break;
+                exit(0);
         }
         printf("Do you want to continue (0: No)? ");
         scanf("%d", &keepGoing);
@@ -176,4 +114,105 @@ int main(){
     
     removeItems();
     return 0;
+}
+
+// Function to make the order
+void makeOrder() {
+    // Display the current cart
+    viewCart();
+
+    // Prompt the user to confirm the order
+    char choice;
+    printf("Do you want to make this order? (Y/N): ");
+    scanf(" %c", &choice);
+
+    // If the user confirms, display a message and clear the cart
+    if (choice == 'Y' || choice == 'y') {
+        printf("Thank you for your order!\n");
+        removeItems();
+    } else {
+        printf("Order canceled.\n");
+    }
+}
+
+void removeItems(){
+    temp = head;
+    while(temp != NULL){
+        removeNode = temp;
+        temp = temp -> next;
+        printf("Removing: %s\n", removeNode -> pizzaName);
+        free(removeNode);
+    }
+    head = tail = NULL; // Reset head and tail pointers
+}
+
+void viewCart(){
+    temp = head;
+    int price = 0;
+    printf("\n--------- Your Cart ---------\n");
+    printf("Name\t\tPrice\n");
+    printf("--------------------------------\n");
+    while(temp != NULL){
+        printf("%-20s %d\n", temp -> pizzaName, temp -> price);
+        price += temp -> price;
+        temp = temp -> next;
+    }
+    printf("--------------------------------\n");
+    printf("Total Cost: %d\n\n", price);
+}
+
+void addItemsToCart(char pizzaName[], int price){
+    newnode = (struct item*)malloc(sizeof(struct item));
+    strcpy(newnode -> pizzaName, pizzaName);
+    newnode -> price = price;
+    newnode -> next = NULL;
+
+    if(head == NULL){
+        head = tail = newnode;
+    }else{
+        tail -> next = newnode;
+        tail = newnode;
+    }
+}
+
+void mainMenu(){
+    printf("\n---------- Main Menu ----------\n");
+    printf("1. Veg Pizza\n");
+    printf("2. Non Veg Pizza\n");
+    printf("3. View Cart\n");
+    printf("4. Make Order\n");
+    printf("5. Exit\n");
+    printf("------------------------------\n");
+}
+
+void vegMenu(){
+    printf("\n--------- Veg Pizza ---------\n");
+    printf("1. Cheese Pizza\n");
+    printf("2. Veggie Pizza\n");
+    printf("3. Pepperoni Pizza\n");
+    printf("4. Back to Main Menu\n");
+    printf("-----------------------------\n");
+}
+
+void nonVegMenu(){
+    printf("\n-------- Non Veg Pizza --------\n");
+    printf("1. BBQ Chicken Pizza\n");
+    printf("2. Meat Pizza\n");
+    printf("3. Gamberi Pizza\n");
+    printf("4. Back to Main Menu\n");
+    printf("-------------------------------\n");
+}
+
+// Function to generate a 16-digit long orderId
+long int generateOrderId() {
+    long int orderId = 0;
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate random digits for the orderId
+    for (int i = 0; i < 16; i++) {
+        orderId = orderId * 10 + rand() % 10;
+    }
+
+    return orderId;
 }
